0f043efe82c12113aed07313cdb6fd13eba9417c
TBGSoff <svla20@student.aau.dk>
2024-03-15 15:44:54 +0100

    Mech
    Mech:
    Lantern
    Document
    Funiture Animations

---
+++ Assets/Soffs Mappe/Scripts/PressKeyPickUpObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressKeyPickUpObject : MonoBehaviour
+{
+    public GameObject Instruction;
+    public GameObject ThisTrigger;
+    public GameObject ObjectOnGround;
+    public GameObject ObjectOnHand;
+    public bool Action = false;
+
+    void Start()
+    {
+        Instruction.SetActive(false);
+        ThisTrigger.SetActive(true);
+        ObjectOnGround.SetActive(true);
+        ObjectOnHand.SetActive(false);
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+                Instruction.SetActive(false);
+                ObjectOnGround.SetActive(false);
+                ObjectOnHand.SetActive(true);
+                ThisTrigger.SetActive(false);
+                Action = false;
+
+            }
+        }
+
+    }
+}
+
